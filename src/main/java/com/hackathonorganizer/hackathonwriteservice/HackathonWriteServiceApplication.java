ae6495414c6d0ebdf401d95eb8e716745d504d25
Tegridy <jarjak4@gmail.com>
2022-07-17 13:46:45 +0200

    Project init

---
+++ src/main/java/com/hackathonorganizer/hackathonwriteservice/HackathonWriteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hackathonorganizer.hackathonwriteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonWriteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonWriteServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathonorganizer/hackathonwriteservice/HackathonWriteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathonorganizer.hackathonwriteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonWriteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
