ba26469873bdbcb8b6546a8c946210373ecb1cb8
mrrevo55 <kwolny31@gmail.com>
2022-10-27 16:30:00 +0200

    Add validation to TeamController.java

--- src/main/java/com/hackathonorganizer/hackathonwriteservice/team/model/dto/TeamInvitationDto.java
+++ src/main/java/com/hackathonorganizer/hackathonwriteservice/team/model/dto/TeamInvitationDto.java
@@ -1,12 +1,17 @@
 package com.hackathonorganizer.hackathonwriteservice.team.model.dto;
 
 import com.hackathonorganizer.hackathonwriteservice.team.model.InvitationStatus;
+
+import javax.validation.constraints.NotEmpty;
+
 public record TeamInvitationDto (
 
     Long id,
+    @NotEmpty
     String fromUserName,
     Long toUserId,
     InvitationStatus invitationStatus,
+    @NotEmpty
     String teamName,
     Long teamId
 
