1ec9e2ad4c1c0990e43e127a46910b7fd4ff981a
Tegridy <jarjak4@gmail.com>
2022-11-07 12:21:27 +0100

    Refactor hackathon controller

--- src/main/java/com/hackathonorganizer/hackathonwriteservice/hackathon/controller/HackathonController.java
+++ src/main/java/com/hackathonorganizer/hackathonwriteservice/hackathon/controller/HackathonController.java
@@ -13,15 +13,12 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/write/hackathons")
 @AllArgsConstructor
-@Slf4j
 public class HackathonController {
     private final HackathonService hackathonService;
 
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
     public HackathonResponse createHackathon(@RequestBody @Valid HackathonRequest hackathon) {
-
-        log.info("Trying to create new hackathon");
 
         return hackathonService.createHackathon(hackathon);
     }
@@ -30,15 +27,11 @@ public class HackathonController {
     public HackathonResponse updateHackathonInfo(@PathVariable("id") Long hackathonId,
             @RequestBody @Valid HackathonRequest hackathonRequest) {
 
-        log.info("Trying to add update hackathon with id {}", hackathonId);
-
         return hackathonService.updateHackathon(hackathonId, hackathonRequest);
     }
 
     @PatchMapping("/{id}/deactivate")
     public void deactivateHackathon(@PathVariable("id") Long hackathonId) {
-
-        log.info("Trying to add deactivate hackathon with id {}", hackathonId);
 
         hackathonService.deactivateHackathon(hackathonId);
     }
@@ -47,16 +40,12 @@ public class HackathonController {
     public void signUpUserToHackathon(@PathVariable("id") Long hackathonId,
             @PathVariable("userId") Long userId) {
 
-        log.info("Trying to add user with id {} to hackathon with id {}", userId, hackathonId);
-
         hackathonService.assignUserToHackathon(hackathonId, userId);
     }
 
     @PatchMapping("/{id}/participants/{userId}/remove")
     public void removeUserFromHackathon(@PathVariable("id") Long hackathonId,
             @PathVariable("userId") Long userId) {
-
-        log.info("Trying to remove user with id {} from hackathon with id {}", userId, hackathonId);
 
         hackathonService.removeUserFromHackathonParticipants(hackathonId, userId);
     }
@@ -66,17 +55,12 @@ public class HackathonController {
     public void addRateCriteriaToHackathon(@PathVariable("id") Long hackathonId,
             @RequestBody List<CriteriaDto> criteriaRequest) {
 
-        log.info("Trying to save criteria to hackathon with id {}", hackathonId);
-
         hackathonService.addRateCriteriaToHackathon(hackathonId, criteriaRequest);
     }
 
     @PutMapping("/{id}/criteria")
     public void updateRateCriteriaToHackathon(@PathVariable("id") Long hackathonId,
             @RequestBody List<CriteriaDto> criteriaRequest) {
-
-        log.info("Trying to update criteria to hackathon with id {}",
-                hackathonId);
 
         hackathonService.updateRateCriteriaToHackathon(hackathonId, criteriaRequest);
     }
@@ -85,15 +69,11 @@ public class HackathonController {
     public void saveTeamRatingAnswer(@PathVariable("id") Long hackathonId,
             @RequestBody List<CriteriaAnswerRequest> criteria) {
 
-        log.info("Trying to save criteria answer to hackathon with id {}", hackathonId);
-
         hackathonService.saveCriteriaAnswers(criteria);
     }
 
     @DeleteMapping("/criteria/{id}")
     public void saveTeamRatingAnswer(@PathVariable("id") Long criteriaId) {
-
-        log.info("Trying to delete criteria with id {}", criteriaId);
 
         hackathonService.deleteCriteria(criteriaId);
     }
--- src/main/java/com/hackathonorganizer/hackathonwriteservice/hackathon/repository/CriteriaRepository.java
+++ src/main/java/com/hackathonorganizer/hackathonwriteservice/hackathon/repository/CriteriaRepository.java
@@ -8,7 +8,5 @@ import java.util.List;
 
 public interface CriteriaRepository extends JpaRepository<Criteria, Long> {
 
-    List<Criteria> findCriteriaByHackathonId(Long hackathonId);
-
     boolean existsCriteriaByNameAndHackathonId(String name, Long hackathonId);
 }
